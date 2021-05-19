df0412db3e7757fd0f6284ad58b10b7c238fc132
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-05-19 20:31:09 -0300

    Adiciona programa 5

---
+++ INTRODUCTION/bloco_4/dia_1/exercicio_5/script.js
@@ -0,0 +1,13 @@
+let x = 60;
+let y = 60;
+let z = 60;
+
+if (x > 0 && y > 0 && z > 0) {
+  if (x + y + z === 180) {
+    console.log('True')
+  } else {
+    console.log('False')
+  }
+} else {
+  console.log('Ocorreu um erro')
+}
