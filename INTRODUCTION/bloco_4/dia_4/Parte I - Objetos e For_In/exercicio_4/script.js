4ea829e2946f1367476f5d70db5988b7656db385
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-05-24 19:35:23 -0300

    Adiciona o titulo no exercicio_3 e adiciona exercicio_4 da Parte I

--- INTRODUCTION/bloco_4/dia_4/Parte I - Objetos e For_In/exercicio_4/script.js
+++ INTRODUCTION/bloco_4/dia_4/Parte I - Objetos e For_In/exercicio_4/script.js
@@ -1,4 +1,4 @@
-//Faça um for/in que mostre todas as chaves do objeto
+//Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto
 
 let info = {
   personagem: 'Margarida',
@@ -8,5 +8,5 @@ let info = {
 };
 
 for (let key in info) {
-  console.log(key);
+  console.log(info[key]);
 }
