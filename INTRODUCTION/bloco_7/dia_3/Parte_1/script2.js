fee15e5fb4691a5b63e7827cd813d00756570029
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-06-21 23:17:20 -0300

    Adiciona script2.js da Parte_1 do bloco 7.3

---
+++ INTRODUCTION/bloco_7/dia_3/Parte_1/script2.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const { type } = require('os');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// 2 - Verifique se o myRemove é uma função
+assert.strictEqual(typeof myRemove, 'function');
+
+// 2.1 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4], 'O array esperado é [1, 2, 4]');
+
+// 2.2 - Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// 2.3 - Verifique se o array passado por parâmetro não sofreu alterações
+
+
+// 2.4 - Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
