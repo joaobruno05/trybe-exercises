dfdfb7986d1cb3e9fca0f153f739d5054a9a77e6
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-12-01 00:27:57 -0300

    Add exercise 3

--- BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/package-npm/imc.js
+++ BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-introducao/package-npm/imc.js
@@ -1,4 +1,9 @@
-const imc = (peso, altura) => {
+const { questionFloat } = require('readline-sync');
+
+const peso = questionFloat('Qual o seu peso?')
+const altura = questionFloat('Qual a sua altura?')
+
+const imc = () => {
   return (peso / Math.pow(altura, 2));
 }
 
