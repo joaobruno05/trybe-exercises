3dd9a8c51c1127603523f12f5070e0787805589a
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-12-02 00:11:51 -0300

    Add exercise 1

---
+++ BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -0,0 +1,26 @@
+function operationNumbers(a, b, c) {
+  const promise = new Promise((resolve, reject) => {
+    const operation = (a + b) * c;
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+      reject(new Error("Informe apenas números"));
+    } else if (operation < 50) {
+      reject(new Error("Valor muito baixo"));
+      } else {
+        resolve(operation);
+      }
+    }
+  );
+  return promise;
+}
+
+operationNumbers(2, 4, 6)
+  .then(result => console.log(`Sucesso: ${result}`))
+  .catch(error => console.log(`Erro: ${error.message}`));
+
+operationNumbers(3, 8, 5)
+  .then(result => console.log(`Sucesso: ${result}`))
+  .catch(error => console.log(`Erro: ${error.message}`));
+
+operationNumbers(2, 4, 'olá')
+  .then(result => console.log(`Sucesso: ${result}`))
+  .catch(error => console.log(`Erro: ${error.message}`));
