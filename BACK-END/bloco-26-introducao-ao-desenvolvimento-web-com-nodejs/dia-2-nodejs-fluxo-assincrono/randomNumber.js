27785f31960ef00fd7fb0204139e29ed552a810f
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-12-02 01:37:05 -0300

    Add exercise 3

--- BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/randomNumber.js
+++ BACK-END/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/randomNumber.js
@@ -10,6 +10,20 @@ function generateRandomNumber() {
   return arrayNumbers;
 }
 
-operationNumbers(...generateRandomNumber())
-  .then(result => console.log(`Sucesso: ${result}`))
-  .catch(error => console.log(`Erro: ${error}`));
+
+// Com then e catch
+// operationNumbers(...generateRandomNumber())
+//   .then(result => console.log(`Sucesso: ${result}`))
+//   .catch(error => console.log(`Erro: ${error}`));
+
+// Com async/await
+const operationAsyncAwait = async () => {
+  try {
+    const result = await operationNumbers(...generateRandomNumber());
+    console.log(`Sucesso: ${result}`);
+  } catch (error) {
+    console.log(`Erro: ${error.message}`);
+  }
+}
+
+operationAsyncAwait();
