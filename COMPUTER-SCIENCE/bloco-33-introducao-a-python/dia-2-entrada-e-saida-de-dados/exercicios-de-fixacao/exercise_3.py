bd64bb3e21f7a884f19637199eb1414a80d0be5d
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-03-22 19:14:25 -0300

    Atualiza exercicio 3 para criar outro arquivo com o filtro aplicado

--- COMPUTER-SCIENCE/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicios-de-fixacao/exercise_3.py
+++ COMPUTER-SCIENCE/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicios-de-fixacao/exercise_3.py
@@ -1,9 +1,13 @@
-file = open("alunos_python.txt", mode="r")
-
 minimum_grade = 6
+new_file = []
 
+file = open("alunos_python.txt", mode="r")
 for line in file:
-    list = line.split()
-    if int(list[1]) < minimum_grade:
-        print(list[0])
+    line = line.split()
+    if int(line[1]) < minimum_grade:
+        new_file.append(line[0])
+
 file.close()
+
+with open("new_file.txt", mode="w") as other_file:
+    other_file.writelines(new_file)
