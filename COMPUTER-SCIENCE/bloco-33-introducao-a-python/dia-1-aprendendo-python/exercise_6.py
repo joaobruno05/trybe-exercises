7716b9a15326aa78de577358ce63345a1d7de94d
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-03-21 23:14:08 -0300

    Modifica funçao do exercicio 6

--- COMPUTER-SCIENCE/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercise_6.py
+++ COMPUTER-SCIENCE/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercise_6.py
@@ -1,5 +1,5 @@
 def triangle_type(a, b, c):
-    if (a + b > c) or (a + c > b) or (b + c > a):
+    if (a + b > c) and (a + c > b) and (b + c > a):
         if a == b == c:
             print("Triângulo equilátero")
         elif (a == b) or (a == c) or (b == c):
@@ -10,4 +10,4 @@ def triangle_type(a, b, c):
         print("Não é triângulo")
 
 
-triangle_type(2, 3, 2)
+triangle_type(2, 3, 0)
