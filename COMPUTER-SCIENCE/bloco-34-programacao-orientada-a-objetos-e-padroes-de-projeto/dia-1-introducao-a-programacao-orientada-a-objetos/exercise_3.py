bbd4f619deeb453bfc70f017bbe358adfa0e0fca
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-04-01 15:23:22 -0300

    Atualiza o exercicio 3

--- COMPUTER-SCIENCE/bloco-34-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/exercise_3.py
+++ COMPUTER-SCIENCE/bloco-34-programacao-orientada-a-objetos-e-padroes-de-projeto/dia-1-introducao-a-programacao-orientada-a-objetos/exercise_3.py
@@ -1,8 +1,9 @@
 import math
 
 
-class Circulo:
-    def __init__(self, raio):
+class Geometria:
+    def __init__(self, nome, raio):
+        self.nome = nome
         self.raio = raio
 
     def calcular_area(self):
@@ -10,7 +11,8 @@ class Circulo:
         return round(area, 2)
 
 
-objeto_circulo = Circulo(2)
+objeto_circulo = Geometria("Círculo", 2)
 
+print(objeto_circulo.nome)
 print(objeto_circulo.raio)
 print(objeto_circulo.calcular_area())
