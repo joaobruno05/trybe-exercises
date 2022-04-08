06c6be3de6d30fff1015612a6c0240243f3060a5
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-04-07 23:48:45 -0300

    Adiciona exercicio 1

---
+++ COMPUTER-SCIENCE/bloco-35-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/exercise_1.py
@@ -0,0 +1,11 @@
+import requests
+
+
+def show_content():
+    base_url = "https://httpbin.org/encoding/utf8"
+    response = requests.get(base_url)
+    content = response.text
+    return content
+
+
+print(show_content())
