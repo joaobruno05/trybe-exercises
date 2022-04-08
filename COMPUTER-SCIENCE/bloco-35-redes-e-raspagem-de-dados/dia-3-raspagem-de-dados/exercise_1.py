27de77ce160847ffb794d8de76bb05e860eacdd3
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-04-08 00:44:40 -0300

    Modifica espaçamento do código do exercicio 1

--- COMPUTER-SCIENCE/bloco-35-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/exercise_1.py
+++ COMPUTER-SCIENCE/bloco-35-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/exercise_1.py
@@ -3,8 +3,10 @@ import requests
 
 def show_content():
     base_url = "https://httpbin.org/encoding/utf8"
+
     response = requests.get(base_url)
     content = response.text
+
     return content
 
 
