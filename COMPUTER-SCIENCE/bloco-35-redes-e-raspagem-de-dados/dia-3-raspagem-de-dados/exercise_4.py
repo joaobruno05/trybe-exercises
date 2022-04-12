8b919924346e5c4f1695792a53e577c3e9db7fd3
João Bruno Vasconcelos <joaobruno91@gmail.com>
2022-04-12 18:44:43 -0300

    Adiciona exercicio 4

---
+++ COMPUTER-SCIENCE/bloco-35-redes-e-raspagem-de-dados/dia-3-raspagem-de-dados/exercise_4.py
@@ -0,0 +1,23 @@
+import requests
+from parsel import Selector
+
+
+def extract_infos_site():
+    base_url = (
+        "http://books.toscrape.com/catalogue/the-grand-design_405/index.html"
+    )
+
+    request = requests.get(base_url)
+    selector = Selector(text=request.text)
+
+    title = selector.css(".product_page h1::text").get()
+    price = selector.css(".product_page .price_color::text").get()[2:]
+    description = selector.xpath(
+        '//*[@id="content_inner"]/article/p/text()'
+    ).get()
+    image = selector.css(".product_page img::attr(src)").get()
+
+    print(f"{title}, {price}, {description}, {image}")
+
+
+extract_infos_site()
